588ac4bece1e424c9a27263a7a12d02845c3785c
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-03-20 14:41:17 +0200

    Add files via upload

--- Getmovies.php
+++ Getmovies.php
@@ -24,7 +24,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> Name: ". $row["MName"]. " - Description: ". $row["MDesc"]."<br>";
+        echo "<br> Name: ". $row["MName"]. " - Description: ". $row["MDesc"].;
     }
 } else {
     echo "0 results";
