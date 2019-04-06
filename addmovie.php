f3131e6de305acd0c006090a07c23e09b45e4214
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-04-06 14:08:43 +0300

    Add files via upload

--- addmovie.php
+++ addmovie.php
@@ -10,7 +10,6 @@ $servername = "127.0.0.1:50367";
 $username = "azure";
 $password = "6#vWHD_$";
 $dbname = "moviedb";
-$lisaa = $_POST["lisaa"];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -18,7 +17,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO movie (MName) VALUES ($lisaa)"
+$sql = "INSERT INTO movie (MName) VALUES ('$_POST[lisaa]')"
 
 if ($conn->query($sql)===TRUE){
     echo "Tallennettu";
