3dc2e9f72926d72cf27eb29abe2dc855dcd9ea32
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-04-10 13:35:39 +0300

    Add files via upload

---
+++ arvostelu.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Getmovies</title>
+</head>
+<body>
+<?php
+$servername = "127.0.0.1:50367";
+$username = "azure";
+$password = "6#vWHD_$";
+$dbname = "moviedb";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "INSERT INTO movie (MDesc) VALUES ('$_POST[arvostelu]')";
+
+
+if ($conn->query($sql)===TRUE){
+   echo "Tallennettu";
+}
+    else{
+        echo "Tallennus epäonnistui";
+    }
+
+
+$conn->close();
+?>
+
+
+</body>
+</html>
