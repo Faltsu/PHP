bbd574293ea44a9d2b7749abf7c3098e863f252e
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-04-10 14:21:18 +0300

    Add files via upload

--- sivu.php
+++ sivu.php
@@ -22,11 +22,6 @@
       <input type="submit" value="Lisää elokuva ja tiedot">
       </form>
 
-      <form action="arvostelu.php" method="post">
-        <input type="text" name="arvostelu">
-        <input type="submit" value="Lisää arvostelu">
-      </form>
-
     </div>
   </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
