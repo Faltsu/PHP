28ce937dfe9b1986a469998418b129fc1b5ba97f
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-04-24 15:34:47 +0300

    Update Searchmovie.php

--- Searchmovie.php
+++ Searchmovie.php
@@ -46,10 +46,10 @@ $conn->close();
 ?>
 <div class="row">
  <div class=col-sm-12>
-   <textarea name="arvostelukentta" form="arvosteluform">Kirjotia arvostelu tähän</textarea>
-   <form action="arvostelu.php" id="arvosteluform">
-     <input type="submit" name="Tallenna arvostelu">
-   </form>
+   <form action="arvostelu.php" id="arvostelu">
+	   <input type="submit">
+	 </form>
+	 <textarea name="arvostelukentta" form="arvostelu">Lisää arvostelu tähän</textarea>
  </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
