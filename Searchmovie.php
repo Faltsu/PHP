93f5fa48e1a18d0a9996dd3ac15ab4368b362809
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-04-24 13:43:08 +0300

    Update Searchmovie.php

--- Searchmovie.php
+++ Searchmovie.php
@@ -27,7 +27,7 @@ $id = $conn->query($sql2);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> Name: ". $row["MName"]. " - Description: ". $row["MDesc"];
+        echo "<br> Name: ". $row["MName"]. " - Description: ". $row["MDesc"] .$["idMovie"];
 	echo $id;
     }
 } else {
