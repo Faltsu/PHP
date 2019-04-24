e9875d6748c20fd6a5c15fd8d6c53165d9fb335e
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-04-24 15:26:40 +0300

    Update arvostelu.php

--- arvostelu.php
+++ arvostelu.php
@@ -19,7 +19,7 @@ if ($conn->connect_error) {
 }
 $id=$_SESSION["id"];
 
-$sql = "INSERT INTO rating (RDesc, Movie_idMovie) VALUES ('$_POST[arvosteluform]', '$id')";
+$sql = "INSERT INTO rating (RDesc, Movie_idMovie) VALUES ('$_POST[arvostelukentta]', '$id')";
 echo "$id";
    
 
