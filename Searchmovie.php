13457957cc0731fe96c33fa454873c390cb218ec
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-04-24 14:48:57 +0300

    Update Searchmovie.php

--- Searchmovie.php
+++ Searchmovie.php
@@ -55,7 +55,7 @@ $conn->close();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </div>
-/*Lähettää ID:N*/
+<!--Lähettää ID:n -->
 <input type='hidden' name='idlahetti' value='<?php serialize($id); ?>' />
 </body>
 </html>
