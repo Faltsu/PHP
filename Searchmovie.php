1bf3a832729ffe40c7d8475dfc935f8e0115f72d
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-04-24 14:11:27 +0300

    Update Searchmovie.php

--- Searchmovie.php
+++ Searchmovie.php
@@ -23,7 +23,7 @@ $sql = "SELECT MName,idMovie, MDesc FROM movie WHERE MName LIKE '%$etsi%' LIMIT 
 $sql2 = "SELECT idMovie, MName FROM movie WHERE MName LIKE '%$etsi%' LIMIT 1";
 $result = $conn->query($sql);
 $id = mysql_query("SELECT idMovie, MName FROM movie WHERE MName LIKE '%$etsi%' LIMIT 1");
-$seppo = $conn->query($id)
+$seppo = $conn->query($id);
 echo $seppo;
  
 	
