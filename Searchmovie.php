3ed655ea0387577dbe6ed828fe7cbeb58dc69b8e
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-05-02 14:13:43 +0300

    Add files via upload

--- Searchmovie.php
+++ Searchmovie.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 <meta charset="utf-8">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<title>Getmovies</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <title>Getmovies</title>
 </head>
 <body>
     <div class="col-sm-12 text-center">
