18242dcf92e2d8425ac6baa58f9b12cbbcaeddb4
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-05-02 14:18:11 +0300

    Add files via upload

--- Searchmovie.php
+++ Searchmovie.php
@@ -36,7 +36,7 @@ $_SESSION["id"] = $id;
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<p id="otsikko"> Elokuvan nimi: ". $row["MName"]. " - Kuvaus: ". $row["MDesc"];
+        echo "<br> Elokuvan nimi: ". $row["MName"]. " - Kuvaus: ". $row["MDesc"];
 	
     }
 } else {
