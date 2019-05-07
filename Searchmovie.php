586c7dd45d78dbb41e0848d3e474993a5d180aee
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-05-07 19:44:36 +0300

    Update Searchmovie.php

--- Searchmovie.php
+++ Searchmovie.php
@@ -44,6 +44,16 @@ $conn->close();
 ?>
 <!-- Otsikko div loppuu-->
 </div>
+<?php 
+    if(empty($etsi)){ 
+      ?>
+          <!-- nappi joka palaa etusivulle --> 
+        <br>    
+        <form action="/sivu.php">
+            <input type="submit" value="Etusivulle">
+<?php
+}  
+    else { ?>
 <div class="col-sm-12 text-center">
       <form action="arvostelu.php" method="post">
       <input type="text" name="arvostelukentta">
@@ -65,6 +75,9 @@ $conn->close();
             <input type="submit" value="Etusivulle">
         </form>
  </div>
+ <?php
+}  
+?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
