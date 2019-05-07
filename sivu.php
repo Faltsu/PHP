6e17babf4211127abebb822027ef2b7e8c7a88c7
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-05-08 00:43:27 +0300

    Add files via upload

--- sivu.php
+++ sivu.php
@@ -21,6 +21,7 @@
           <br>
           Lisää elokuva painamalla "Lisää elokuva"</p>
       </div>
+      <!-- Nappulat-->
     <div class="col-sm-12 text-center">
       <form action="Searchmovie.php" method="post">
       <input type="text" name="etsi">
@@ -33,7 +34,7 @@
         <input type="submit" value="Listaa elokuvat"/>
       </form>
     </div>
-    </div>
+    <!--Footer-->
     <div class="footer">
       <div class="col-sm-12 text-center">
         <p id="alatunniste">
@@ -41,6 +42,9 @@
         </p>
       </div>
     </div>
+    <!--container loppuu-->
+    </div>
+    
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
