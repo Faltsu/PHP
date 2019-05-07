20317feca35475347e6221f61041a8f002d53a23
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-05-07 17:17:16 +0300

    Update arvostelu.php

--- arvostelu.php
+++ arvostelu.php
@@ -9,16 +9,7 @@
 <div class="col-sm-12 text-center">
 <?php
 session_start();
-$servername = "127.0.0.1:50367";
-$username = "azure";
-$password = "6#vWHD_$";
-$dbname = "moviedb";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include_once 'conn.php';
 $id=$_SESSION["id"];
 
 $sql = "INSERT INTO rating (RDesc, Movie_idMovie) VALUES ('$_POST[arvostelukentta]', '$id')";
