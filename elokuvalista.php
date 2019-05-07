4c57f0e4a858306bbb7c19f6c6626d2978c18c35
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-05-07 17:36:33 +0300

    Update elokuvalista.php

--- elokuvalista.php
+++ elokuvalista.php
@@ -29,12 +29,12 @@ if ($result->num_rows > 0) {
 } else {
     echo "EI LÖYDY ELOKUVAA!";
 }
-      <!-- nappi joka palaa etusivulle -->
+$conn->close();
+?>
+          <!-- nappi joka palaa etusivulle -->
         <form action="/sivu.php">
             <input type="submit" value="Etusivulle">
 </form>
-$conn->close();
-?>
 <!--css div loppuu-->
 </div>
 </body>
