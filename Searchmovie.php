7742360eb90e61e1633ff3bc8e8c839b1cf74e51
KimiLaakso <43385087+KimiLaakso@users.noreply.github.com>
2019-05-08 00:56:08 +0300

    Add files via upload

--- Searchmovie.php
+++ Searchmovie.php
@@ -10,7 +10,7 @@
 <div class="header">
   <div class="col-sm-12 text-center">
     <p id="oikeeotsikko">
-      Elokuvadatabase1
+      Elokuvadatabase222
     </p>
   </div>
 </div>
@@ -66,6 +66,7 @@ $conn->close();
 }  
     else { ?>
     </div>
+
 <div class="col-sm-12 text-center">
   <form action="/arvostelusivu.php">
     <input type="submit" value="Katso elokuvan arvostelut">
@@ -89,21 +90,24 @@ $conn->close();
         <form action="/sivu.php">
             <input type="submit" value="Etusivulle">
         </form>
- </div>
- <!--Footer-->
- <div class="footer">
+ <?php
+}  
+?>
+</div>
+</div>
+<!--Footer-->
+<div class="footer">
       <div class="col-sm-12 text-center"> 
         <p>Tehnyt Kim Laakso ja Jarkko Hämäläinen</p>
       </div>
     </div>
     <!--container loppuu-->
     </div>
- <?php
-}  
-?>
+
+</div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</div>
+
 </body>
 </html>
