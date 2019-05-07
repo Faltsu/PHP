257460db78b89bf42c08b8541ee654d96ec00e08
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-05-07 19:05:27 +0300

    Update Searchmovie.php

--- Searchmovie.php
+++ Searchmovie.php
@@ -18,7 +18,7 @@ $sql2 = "SELECT idMovie, MName FROM movie WHERE MName LIKE '%$etsi%' LIMIT 1";
 $result = $conn->query($sql);
 $result2=mysqli_query($conn,$sql2);
 
-// Numeric array
+// Tallenna id arvostelua varten
 $row=mysqli_fetch_array($result2,MYSQLI_NUM);
 $id=$row[0];
 $_SESSION["id"] = $id;
@@ -51,16 +51,19 @@ $conn->close();
     <option value="9">9</option>
     <option value="10">10</option>
   </select>
-      <input type="text" name="arvostelukentta2">
       <input type="submit" value="Arvostele elokuva">
       </form>
       <!-- nappi joka palaa etusivulle -->
         <form action="/sivu.php">
             <input type="submit" value="Etusivulle">
         </form>
-
-	 
  </div>
+<?php
+if(!isset($_POST['arvosana']))
+  {
+    $errorMessage .= "<li>Et antanut arvosanaa!</li>";
+  }
+?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
