fd794b70ea6a386af8c3538d5155f6965458f53c
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-05-07 15:19:33 +0300

    Update sivu.php

--- sivu.php
+++ sivu.php
@@ -21,6 +21,9 @@
       <form action="/sivu2.php">
         <input type="submit" value="Lisää elokuva"/>
       </form>
+            <form action="/elokuvalista.php">
+        <input type="submit" value="Lista kaikista elokuvista"/>
+      </form>
     </div>
  
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
